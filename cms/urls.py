2f3de12b6f624924500a9aee133720890a2d9193
Jess Johnson <jess@grok-code.com>
2012-03-26 00:13:07 -0600

    Initial import of sources. Code through chapter 4.

---
+++ cms/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^cms/', include('cms.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),                       
+    (r'^admin/', include(admin.site.urls)),
+    (r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
+     { 'document_root': '/home/smudge/work/django-practice/cms/scripts/tiny_mce/' }),
+    (r'^search/$', 'cms.search.views.search'),
+    (r'^weblog/', include('coltrane.urls')),
+    (r'', include('django.contrib.flatpages.urls')),
+)
---
+++ coltrane/models.py
@@ -0,0 +1,86 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+from tagging.fields import TagField
+from markdown import markdown
+
+class Category(models.Model):
+    title = models.CharField(max_length=250, 
+                             help_text='Maximum 250 characters')
+    slug = models.SlugField(unique=True, 
+                            help_text='Suggested value automatically generated from title. Must be unique.')
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "categories/%s/ % self.slug"
+
+
+
+class Entry(models.Model):
+    
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
+    STATUS_CHOICES = (
+        (LIVE_STATUS, 'Live'),
+        (DRAFT_STATUS, 'Draft'),
+        (HIDDEN_STATUS, 'Hidden'),
+    )
+
+    # Core fields.
+    title = models.CharField(max_length=250,
+                             help_text='Maximum 250 characters')
+    excerpt = models.TextField(blank=True)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+
+    # Fields to store generated html.
+    excerpt_html = models.TextField(editable=False, blank=True)
+    body_html = models.TextField(editable=False, blank=True)
+
+    # Metadata.
+    slug = models.SlugField(unique_for_date='pub_date')
+    author = models.ForeignKey(User)
+    enable_comments = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+ 
+    # Categorization.
+    categories = models.ManyToManyField(Category)
+    tags = TagField()
+
+
+    class Meta:
+        verbose_name_plural = "Entries"
+        ordering = ['-pub_date']
+
+    
+    def __unicode__(self):
+        return self.title
+    
+
+    def save(self, force_insert=False, force_update=False):
+        self.body_html = markdown(self.body)
+        if self.excerpt:
+            self.excerpt_html = markdown(self.excerpt)
+        super(Entry, self).save(force_insert, force_update)
+
+        
+    def get_absolute_url(self):
+        return ('coltrane_entry_detail', (), { 'year' : self.pub_date.strftime("%Y"),
+                                               'month' : self.pub_date.strftime("%b").lower(),
+                                               'day' : self.pub_date.strftime("%d"),
+                                               'slug' : self.slug })
+    
+    
+    get_absolute_url = models.permalink(get_absolute_url)
+    
+        
+
