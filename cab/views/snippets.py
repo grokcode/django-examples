7a5c877c96da80fff6a92050c462bac0f9ee73f9
Jess Johnson <jess@grok-code.com>
2012-04-03 00:02:48 -0600

    Create form for submitting snippets.

---
+++ cab/views/snippets.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponseRedirect
+from django.forms import ModelForm
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from cab.models import Snippet
+
+
+
+class SnippetForm(ModelForm):
+    class Meta:
+        model = Snippet
+        exclude = ['author']
+
+
+
+def add_snippet(request):
+    if request.method == 'POST':
+        form = SnippetForm(data=request.POST)
+        if form.is_valid():
+            new_snippet = form.save(commit=False)
+            new_snippet.author = request.user
+            new_snippet.save()
+            return HttpResponseRedirect(new_snippet.get_absolute_url())
+    else:
+        form = SnippetForm()
+    return render_to_response('cab/add_snippet.html', { 'form': form })
+
+add_snippet = login_required(add_snippet)
+
+
+
